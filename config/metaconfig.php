6109dfe206b918d694354868ebe46afaaa4a0fbd
cmb69 <cmbecker69@gmx.de>
2013-11-26 15:09:10 +0000

    added metaconfig.php for XH 1.6

---
+++ config/metaconfig.php
@@ -0,0 +1,10 @@
+<?php
+
+$plugin_mcf['advancedform']['mail_captcha']="bool";
+$plugin_mcf['advancedform']['mail_type']="enum:html,text";
+$plugin_mcf['advancedform']['mail_line_ending_*nix']="bool";
+$plugin_mcf['advancedform']['focus_form']="bool";
+$plugin_mcf['advancedform']['datepicker_icon']="bool";
+$plugin_mcf['advancedform']['php_extension']="bool";
+
+?>
