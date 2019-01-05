3c18c2fd03beda028e55605e397e4d8fba90be15
Christoph M. Becker <cmbecker69@gmx.de>
2019-01-05 15:10:19 +0100

    Hard-code version number
    This solves misleading update-check information for development
    checkouts, and also serves as preliminary step for hosting the server
    side version.nfo on Github.

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 /**
  * The version number.
  */
-define('ADVANCEDFORM_VERSION', '@ADVANCEDFORM_VERSION@');
+define('ADVANCEDFORM_VERSION', '1.1');
 
 /**
  * Main plugin call.
