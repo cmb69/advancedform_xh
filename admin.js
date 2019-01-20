944140fd54bd3e711fabecdae01d0b2df5047a55
Christoph M. Becker <cmbecker69@gmx.de>
2019-01-20 15:16:28 +0100

    Use GNU style file headers

--- admin.js
+++ admin.js
@@ -1,9 +1,22 @@
-/*
- * Form editor of Advancedform_XH.
- * Copyright (c) 2005-2010 Jan Kanters
- * Copyright (c) 2011-2019 Christoph M. Becker (see license.txt)
+/**
+ * Copyright 2005-2010 Jan Kanters
+ * Copyright 2011-2019 Christoph M. Becker
+ *
+ * This file is part of Advancedform_XH.
+ *
+ * Advancedform_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Advancedform_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Advancedform_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 /**
  * Property indexes.
--- tests/unit/CsvTest.php
+++ tests/unit/CsvTest.php
@@ -1,16 +1,22 @@
 <?php
 
 /**
- * Testing the CSV related functionality.
+ * Copyright 2014-2019 Christoph M. Becker
  *
- * PHP version 5
+ * This file is part of Advancedform_XH.
  *
- * @category  Testing
- * @package   Advancedform
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2014-2019 Christoph M. Becker <http://3-magi.net>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Advancedform_XH
+ * Advancedform_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Advancedform_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Advancedform_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once './vendor/autoload.php';
