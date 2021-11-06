9395f26b691f2bf8651086e11676045b02bdc8ae
Christoph M. Becker <cmbecker69@gmx.de>
2021-11-06 13:36:53 +0100

    Clean up config.php
    The canonical values for booleans are `true`/`` for a long time.  We
    also properly escape `mail_regexp`, and remove the superfluous closing
    PHP tag.

--- config/config.php
+++ config/config.php
@@ -5,16 +5,14 @@ $plugin_cf['advancedform']['mail_to_name']="Webmaster";
 $plugin_cf['advancedform']['mail_to']="webmaster@example.com";
 $plugin_cf['advancedform']['mail_cc']="";
 $plugin_cf['advancedform']['mail_bcc']="";
-$plugin_cf['advancedform']['mail_captcha']="0";
+$plugin_cf['advancedform']['mail_captcha']="";
 $plugin_cf['advancedform']['mail_thanks_page']="";
-$plugin_cf['advancedform']['mail_regexp']="/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i";
+$plugin_cf['advancedform']['mail_regexp']="/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\$/i";
 $plugin_cf['advancedform']['mail_type']="html";
 $plugin_cf['advancedform']['mail_confirmation']="";
 $plugin_cf['advancedform']['required_field_mark']="%s&nbsp;<span style=\"color:red\">*</span>";
-$plugin_cf['advancedform']['focus_form']="1";
+$plugin_cf['advancedform']['focus_form']="true";
 $plugin_cf['advancedform']['captcha_plugin']="advancedform";
 $plugin_cf['advancedform']['captcha_key']="";
-$plugin_cf['advancedform']['php_extension']="0";
+$plugin_cf['advancedform']['php_extension']="";
 $plugin_cf['advancedform']['csv_separator']="";
-
-?>
