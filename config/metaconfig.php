5c01318d3a35c2e2753a53ec2d79bb0ae254afbf
Christoph M. Becker <cmbecker69@gmx.de>
2025-02-27 12:14:57 +0100

    Use password field type for password
    That is ignored prior to CMSimple_XH 1.8.0, so we're still compatible.

--- config/metaconfig.php
+++ config/metaconfig.php
@@ -6,6 +6,7 @@ $plugin_mcf['advancedform']['mail_confirmation']="bool";
 $plugin_mcf['advancedform']['smtp_enabled']="bool";
 $plugin_mcf['advancedform']['smtp_port']="xenum:25,587,465";
 $plugin_mcf['advancedform']['smtp_tls']="enum:,STARTTLS,SMTPS";
+$plugin_mcf['advancedform']['smtp_password']="password";
 $plugin_mcf['advancedform']['smtp_debug']="bool";
 $plugin_mcf['advancedform']['focus_form']="bool";
 $plugin_mcf['advancedform']['captcha_key']="random";
